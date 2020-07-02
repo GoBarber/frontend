876f6c2b10b33c00321c7cfe3af7aecc3b647693
Bernardo Henrique <bernardohrl.unb@gmail.com>
2020-07-02 17:22:12 -0300

    LifeCycles
    Also using localstorage

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -3,15 +3,34 @@ import TechItem from './TechItem';
 
 class TechList extends Component {
   static defaultProps = { /* Poderia colocar desta maneira também. */ };
-  static PropTypes = { /* Poderia colocar desta maneira também. */ };
+  static propTypes = { /* Poderia colocar desta maneira também. */ };
 
   state = {
     newTech: '',
-    techs: [
-      'node',
-      'react', 
-      'native'
-    ]
+    techs: [ ]
+  }
+
+  // Executado assim que o component aparece na tela
+  // Um exemplo de utilização é fazer chamada para uma API para, com os valores, montar o componente
+  componentDidMount() { 
+    const techs = localStorage.getItem('techs');
+
+    if(techs) {
+      this.setState({ techs: JSON.parse(techs) });
+    }
+  }
+
+  // Chamado sempre quando uma Prop ou State é alterado.
+  // Os estados/props antigos são passados como parâmetro (prevProps e prevState)
+  componentDidUpdate(_, prevState) {  // O _ é um padrão para indicar que não será usado
+    if(prevState.tech !== this.state.techs) {
+      localStorage.setItem('techs', JSON.stringify(this.state.techs));
+    }
+  }
+
+  // Executado quando o componente vai ser deletado.
+  componentWillUnmount() {
+
   }
 
   handleInputChange = e => {
@@ -21,7 +40,6 @@ class TechList extends Component {
 
   handleSubmit = e => {
     e.preventDefault();
-    console.log(this.state.newTech)
 
     this.setState({
       techs: [...this.state.techs, this.state.newTech],
@@ -37,7 +55,6 @@ class TechList extends Component {
     return (
       <form onSubmit={this.handleSubmit}>
         <ul>
-          <h1>{this.state.newTech}</h1>
           {this.state.techs.map(tech => (
             <TechItem
               key={tech}
@@ -45,10 +62,6 @@ class TechList extends Component {
               onDelete={ () => this.handleDelete(tech) }
             />
           ))}
-          <TechItem
-              key={"Oculto"}
-              onDelete={ () => this.handleDelete(tech) }
-            />
         </ul>
         <input type='text' onChange={this.handleInputChange} value={this.state.newTech}/> 
         <button type="submit">enviar</button>
