aa9e7345b567c73cdbfa43301d9de7ca24e6d30b
Bernardo Henrique <bernardohrl.unb@gmail.com>
2020-07-23 18:45:11 -0300

    Stuling
    Highlight fucused input and changes icon when it is filled

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,4 +1,10 @@
-import React, { InputHTMLAttributes, useEffect, useRef } from 'react';
+import React, {
+  InputHTMLAttributes,
+  useEffect,
+  useRef,
+  useState,
+  useCallback,
+} from 'react';
 import { IconBaseProps } from 'react-icons';
 import { useField } from '@unform/core';
 
@@ -11,8 +17,21 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 
 // Converte o atributo icon em letra maiuscula para que possa entender que é um componente.
 const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
   const { fieldName, defaultValue, error, registerField } = useField(name);
+
+  // Porque usar useCallBack?
+  // No react, toda fez que uma função (ou componente) tem mudança de estado ou props, ele é renderizado novamente
+  // Isso quer dizer que a função hadleInputBlur seria adicionada à memória novamente muitas vezes
+  // O useCallback indica quando ela função deve ser adicionada/reescrita, neste caso, [] => apenas no começo
+  const hadleInputBlur = useCallback(() => {
+    setIsFocused(false);
+    setIsFilled(!!inputRef.current?.value);
+  }, []);
+
+  const hadleInputFocus = useCallback(() => setIsFocused(true), []);
 
   useEffect(() => {
     registerField({
@@ -23,9 +42,15 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
   }, [fieldName, registerField]);
 
   return (
-    <Container>
+    <Container isFilled={isFilled} isFocused={isFocused}>
       {Icon && <Icon size={20} />}
-      <input defaultValue={defaultValue} ref={inputRef} {...rest} />
+      <input
+        onFocus={() => hadleInputFocus()}
+        onBlur={() => hadleInputBlur()}
+        defaultValue={defaultValue}
+        ref={inputRef}
+        {...rest}
+      />
     </Container>
   );
 };
