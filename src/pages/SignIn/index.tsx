9cd7507125d13e09be468c9e2ae70ed4b41abb9e
Bernardo Henrique <bernardohrl.unb@gmail.com>
2020-07-23 17:17:49 -0300

    Creates Components
    Creates components for inputs and buttons.

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 import logo from '../../assets/logo.svg';
 import { Container, Content, Background } from './styles';
 
@@ -12,9 +14,14 @@ export const SignIn: React.FC = () => (
       <form>
         <h1> Faça seu logon</h1>
 
-        <input placeholder="Email" />
-        <input type="password" placeholder="Senha" />
-        <button type="submit">Entrar</button>
+        <Input name="email" icon={FiMail} placeholder="Email" />
+        <Input
+          name="password"
+          icon={FiLock}
+          type="password"
+          placeholder="Senha"
+        />
+        <Button type="submit">Entrar</Button>
 
         <a href="forgot">Esqueci minha senha</a>
       </form>
--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -23,44 +23,11 @@ export const Content = styled.div`
   form {
     margin: 80px 0;
     max-width: 340px;
+    /* min-width: 50%; */
     text-align: center;
 
     h1 {
       margin-bottom: 24px;
-    }
-
-    input {
-      & + input {
-        margin-top: 8px;
-      }
-
-      background: #232129;
-      border-radius: 10px;
-      border: 2px solid #232129;
-      padding: 16px;
-      width: 100%;
-
-      color: #f4ede8;
-      box-sizing: border-box;
-    }
-
-    button {
-      background: #ff9000;
-      color: #312e38;
-      font-weight: 500;
-
-      width: 100%;
-      height: 56px;
-      border-radius: 10px;
-      border: 0;
-      padding: 0 16px;
-      margin-top: 16px;
-
-      transition: background-color 0.2s;
-
-      &:hover {
-        background: ${shade(0.2, '#ff9000')};
-      }
     }
 
     a {
